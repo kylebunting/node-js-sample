5de0a87bac8140d58acfda25506e4a8165c2dd60
unknown <kyle.bunting@gmail.com>
2018-01-26 04:36:39 +0000

    1231

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello W-o-r-l-d!')
+  response.send('Hell-o W-o-r-l-d!')
 })
 
 app.listen(app.get('port'), function() {
